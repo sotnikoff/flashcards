ba70476c0b824e3f6b8da70fbaf6c3f4254510a3
Unknown <thehofch@gmail.com>
2018-03-02 14:20:30 +0300

    nokogiri+seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://1000mostcommonwords.com/1000-most-common-german-words/'
+doc = Nokogiri::HTML(open(url))
+doc.css('.entry-content table tbody tr').each_with_index do |row, index|
+  next if index.zero?
+  Card.create({
+      original_text: row.search('td')[1].text.strip,
+      translated_text: row.search('td')[2].text.strip })
+end
