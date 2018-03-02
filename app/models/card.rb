3aa0ad208b8f7aa2dce9494deacfaeb08846c7fc
Unknown <thehofch@gmail.com>
2018-03-02 13:01:34 +0300

    improve validator

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,18 +2,16 @@ class Card < ApplicationRecord
 
   validates :original_text, presence: true
   validates :translated_text, presence: true
-  validate :check_text
+  validate do
+    errors.add(:original_text, :not_same_word) if compare_text
+    errors.add(:translated_text, :not_same_word) if compare_text
+  end
 
   before_create do
     self.review_date = 3.days.since(Time.now)
   end
 
-  def check_text
-    errors.add(:original_text, :not_same_word) if compare_text
-    errors.add(:translated_text, :not_same_word) if compare_text
-  end
-
   def compare_text
-    original_text == translated_text
+    original_text.strip.casecmp(translated_text.strip).zero?
   end
 end
