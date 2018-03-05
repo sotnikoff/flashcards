d7159954acd402d7fe792651acafa39ba800b9fd
Unknown <thehofch@gmail.com>
2018-03-05 12:00:15 +0300

    CheckAnswer fix

--- app/interactors/check_answer.rb
+++ app/interactors/check_answer.rb
@@ -1,11 +1,7 @@
-class CheckAnswer
-
+module CheckAnswer
   def self.call(compare_object, answer)
-    puts compare_object.translated_text
-    puts answer
-    puts compare_answers(compare_object.translated_text, answer)
     if compare_answers(compare_object.translated_text, answer)
-      compare_object.update(review_date: 3.days.since(Time.now))
+      compare_object.update(review_date: 3.days.since)
       I18n.t('answers.create.correct')
     else
       I18n.t('answers.create.wrong')
