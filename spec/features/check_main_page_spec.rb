8cd28b9d557ac522a39a273cb9b697bc255ca92d
Unknown <thehofch@gmail.com>
2018-03-06 09:39:37 +0300

    add capybara test

---
+++ spec/features/check_main_page_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'the answering process' do
+  let(:create_card) do
+    card = create :card
+    card.update(review_date: Time.now)
+    card
+  end
+
+  scenario 'visit main page' do
+    create_card
+    visit '/'
+    expect(page).to have_content 'Zuhause'
+  end
+
+  scenario 'answer with translated text' do
+    card = create_card
+    visit '/'
+    within '#answers-form' do
+      fill_in 'answer', with: card.translated_text
+    end
+    click_button I18n.t('answers.new.button')
+    expect(page).to have_content I18n.t('answers.create.correct')
+  end
+end
