f1fecba6abe2343ac1692f7c74af7ac87b5328d2
Unknown <thehofch@gmail.com>
2018-03-06 16:29:54 +0300

    add auth basic controller + class

---
+++ app/operations/authenticate.rb
@@ -0,0 +1,5 @@
+class Authenticate
+  def self.test
+    puts 123
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: %i[new create destroy]
   resources :cards
   resources :answers, only: %i[new create]
 
