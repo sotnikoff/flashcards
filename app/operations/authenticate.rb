8305c0739864554ff8555697a57f9c33fa6ef372
Unknown <thehofch@gmail.com>
2018-03-08 18:00:14 +0300

    basic password comparing with BCrypt

--- app/operations/authenticate.rb
+++ app/operations/authenticate.rb
@@ -1,5 +1,7 @@
-class Authenticate
-  def self.test
-    puts 123
+require 'bcrypt'
+
+module Authenticate
+  def self.auth(auth_model, password)
+    BCrypt::Password.new(auth_model.hash_password) == password
   end
 end
---
+++ spec/operations/authenticate_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'bcrypt'
+
+RSpec.describe Authenticate do
+  describe 'Checking password' do
+    class ExampleModel
+      attr_accessor :hash_password
+      def initialize(password)
+        @hash_password = BCrypt::Password.create(password)
+      end
+    end
+
+    it 'returns true when password is correct' do
+      test_model = ExampleModel.new('123456')
+      result = Authenticate.auth(test_model,'123456')
+      expect(result).to eq(true)
+    end
+  end
+end
