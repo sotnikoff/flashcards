820473253f074688198e62b4f5e63fd643fba18d
Unknown <thehofch@gmail.com>
2018-03-10 16:24:17 +0300

    change to lib

--- app/operations/authenticate.rb
+++
@@ -1,7 +0,0 @@
-require 'bcrypt'
-
-module Authenticate
-  def self.call(hash, password)
-    BCrypt::Password.new(hash).is_password?(password)
-  end
-end
--- app/operations/create_password.rb
+++
@@ -1,7 +0,0 @@
-require 'bcrypt'
-
-module CreatePassword
-  def self.call(unencrypted_password)
-    BCrypt::Password.create(unencrypted_password)
-  end
-end
