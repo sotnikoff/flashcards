62f3c6bf96b8b0a3909cbfae5083693e261c17b2
Unknown <thehofch@gmail.com>
2018-04-08 23:45:30 +0300

    change autoload

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module Flashcards
     config.load_defaults 5.1
     config.i18n.default_locale = :ru
     config.time_zone = 'Europe/Moscow'
-    config.autoload_paths += %W(#{config.root}/lib)
+    # config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
