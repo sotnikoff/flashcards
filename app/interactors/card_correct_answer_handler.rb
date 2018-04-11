255beb4c3b3e25a94a583773fe4e1498ae6ac10c
Unknown <thehofch@gmail.com>
2018-04-11 11:18:40 +0300

    refactor

--- app/interactors/card_correct_answer_handler.rb
+++ app/interactors/card_correct_answer_handler.rb
@@ -1,13 +1,17 @@
 module CardCorrectAnswerHandler
   extend self
 
-  REVIEW_DATE_VALUES = [0, 12.hours, 3.days, 1.week, 2.weeks, 1.month].freeze
+  REVIEW_DATE_VALUES = [12.hours, 3.days, 1.week, 2.weeks, 1.month].freeze
 
   def call(card)
+    card if card.update(step: sum_step(card),
+                        fails: 0,
+                        review_date: card.review_date += REVIEW_DATE_VALUES[card.step])
+  end
+
+  private
+
+  def sum_step(card)
     card.step += 1 unless card.step >= 4
-    card.fails = 0
-    card.review_date += REVIEW_DATE_VALUES[card.step]
-    card.save
-    card
   end
 end
