9dac30b9141411ac1fdec0bce4297c9775651260
Unknown <thehofch@gmail.com>
2018-04-12 14:35:34 +0300

    MISTAKES_RATIO

--- app/interactors/check_answer.rb
+++ app/interactors/check_answer.rb
@@ -1,5 +1,8 @@
 module CheckAnswer
   extend self
+
+  MISTAKES_RATIO = 0.15
+
   def call(compare_object, answer)
     if compare_answers(compare_object.translated_text, answer)
       CardCorrectAnswerHandler.call(compare_object)
@@ -13,18 +16,12 @@ module CheckAnswer
   private
 
   def compare_answers(translated_text, answer)
-    mistakes = calculate_mistakes(translated_text)
+    mistakes = calculate_mistakes(translated_text.length)
     DamerauLevenshtein.distance(prepare(translated_text), prepare(answer)) <= mistakes
   end
 
-  def calculate_mistakes(string)
-    if string.length.between?(1, 3)
-      0
-    elsif string.length.between?(4, 8)
-      1
-    else
-      2
-    end
+  def calculate_mistakes(length)
+    (length * MISTAKES_RATIO).round
   end
 
   def prepare(string)
