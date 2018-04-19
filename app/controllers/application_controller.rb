c376a5487a772247a639bf5916eedd419116f902
Unknown <thehofch@gmail.com>
2018-04-19 14:05:10 +0300

    translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :set_locale
 
   NotAuthorized = Class.new(StandardError)
 
@@ -12,5 +13,15 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.where(id: session[:current_user_id]).first
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  get 'sign_up', to: 'registrations#new', as: 'sign_up_page'
-  post 'registrations', to: 'registrations#create', as: 'sign_up'
+  scope '/:locale' do
+    get 'sign_up', to: 'registrations#new', as: 'sign_up_page'
+    post 'registrations', to: 'registrations#create', as: 'sign_up'
 
-  get 'login', to: 'sessions#new', as: 'login_page'
-  post 'sessions', to: 'sessions#create', as: 'login'
-  delete 'session', to: 'sessions#destroy', as: 'logout'
+    get 'login', to: 'sessions#new', as: 'login_page'
+    post 'sessions', to: 'sessions#create', as: 'login'
+    delete 'session', to: 'sessions#destroy', as: 'logout'
 
-  scope 'github' do
-    get 'new', to: 'github_sessions#new', as: 'github_new'
-    get 'callback', to: 'github_sessions#callback', as: 'github_callback'
+    scope 'github' do
+      get 'new', to: 'github_sessions#new', as: 'github_new'
+      get 'callback', to: 'github_sessions#callback', as: 'github_callback'
+    end
+
+    resources :cards
+    resources :decks
+    resources :answers, only: %i[new create]
+    resources :users, only: %i[update]
+
+    root to: 'answers#new'
   end
-
-  resources :cards
-  resources :decks
-  resources :answers, only: %i[new create]
-  resources :users, only: %i[update]
-
   root to: 'answers#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
