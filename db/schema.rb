78df200f448c5e30a7123254f2e7d8a7c5c613cf
Unknown <thehofch@gmail.com>
2018-04-19 17:02:21 +0300

    add locale to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410062203) do
+ActiveRecord::Schema.define(version: 20180419140106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180410062203) do
     t.datetime "updated_at", null: false
     t.string "github_token"
     t.bigint "deck_id"
+    t.string "locale"
     t.index ["deck_id"], name: "index_users_on_deck_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
