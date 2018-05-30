04df8ff367cfe207160c71d75007ef9dbb659e1a
Unknown <thehofch@gmail.com>
2018-05-30 14:08:44 +0300

    remove useless

--- app/interactors/card_fail_handler.rb
+++
@@ -1,7 +0,0 @@
-module CardFailHandler
-  extend self
-  def call(card)
-    return card if card.fails >= 3 && card.update(fails: 0, step: 0)
-    card if card.update(fails: card.fails + 1)
-  end
-end
